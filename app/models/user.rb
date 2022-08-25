24a4b7028d23b25ff0b88fd0b5aefcf820c5ecbd
Justin Hurstwright <jryanhurst@gamil.com>
2022-08-25 17:21:29 -0500

    feat: Validate user has email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   devise :trackable, :omniauthable, omniauth_providers: [:google_oauth2]
 
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
 
   def self.from_google(email:, full_name:, uid:, avatar_url:)
     create_with(uid: uid, username: full_name, avatar_url: avatar_url).find_or_create_by!(email: email)
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,4 +6,12 @@ class UserTest < ActiveSupport::TestCase
     refute new_user.valid?, "user requires a username"
     assert_not_nil new_user.errors[:name], "raises an name error"
   end
+
+  test "the email is unique" do
+    existing_user = users(:other)
+    assert_equal(existing_user.email, "other@example.com")
+    new_user = User.new(email: "other@example.com")
+    refute new_user.valid?, "User email cannot be duplicated"
+  end
+
 end
