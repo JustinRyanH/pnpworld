4740073ff0cbc625e01106cdd8b6e8fb70f32567
Justin Hurstwright <jryanhurst@gamil.com>
2022-08-25 10:13:25 -0500

    feat: Validate user has email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   devise :trackable, :omniauthable, omniauth_providers: [:google_oauth2]
 
-  def self.from_google(email:, full_name:, uid:, avatar_url:)
+  validates :email, presence: true
 
+  def self.from_google(email:, full_name:, uid:, avatar_url:)
     create_with(uid: uid, username: full_name, avatar_url: avatar_url).find_or_create_by!(email: email)
   end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,9 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the email is required" do
+    new_user = User.new()
+    refute new_user.valid?, "user requires a username"
+    assert_not_nil new_user.errors[:name], "raises an name error"
+  end
 end
