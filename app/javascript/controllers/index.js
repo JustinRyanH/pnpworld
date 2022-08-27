c87392df5061b0bd027b376339bbd880283073a7
Justin Hurstwright <jryanhurst@gamil.com>
2022-08-27 15:17:47 -0500

    feat: connect to Stimulus component

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -5,4 +5,7 @@
 import { application } from "./application"
 
 import HelloController from "./hello_controller"
+import NavbarController from "./navbar_controller"
+
+application.register("navbar", NavbarController)
 application.register("hello", HelloController)
