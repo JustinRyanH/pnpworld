587ab719e1f9694971ddb8ecfae59e3f43747a50
Justin Hurstwright <jryanhurst@gamil.com>
2022-08-27 20:38:20 -0500

    ref: Extract method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,24 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
   before_action :set_live_reload
 
   NavigationBtn = Struct.new(:active, :link, :text, keyword_init: true)
 
-
   private
+
   helper_method :navigation_buttons
 
   # @return [Array<NavigationBtn>]
   def navigation_buttons
     [
       NavigationBtn.new(active: true, link: root_path, text: 'Home'),
-      NavigationBtn.new(active: false, link: new_user_session_path, text: 'Login')
+      login_logout_btn
     ]
+  end
+
+  def login_logout_btn
+    NavigationBtn.new(active: false, link: new_user_session_path, text: 'Login')
   end
 
   def set_live_reload
