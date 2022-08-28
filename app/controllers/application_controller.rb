1254ce1d994f04d2b83c92073086fe8b4fc305f5
Justin Hurstwright <jryanhurst@gamil.com>
2022-08-27 20:37:53 -0500

    ref: Extract Nav buttons to methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,19 @@
 class ApplicationController < ActionController::Base
   before_action :set_live_reload
 
+  NavigationBtn = Struct.new(:active, :link, :text, keyword_init: true)
+
+
   private
+  helper_method :navigation_buttons
+
+  # @return [Array<NavigationBtn>]
+  def navigation_buttons
+    [
+      NavigationBtn.new(active: true, link: root_path, text: 'Home'),
+      NavigationBtn.new(active: false, link: new_user_session_path, text: 'Login')
+    ]
+  end
 
   def set_live_reload
     @live_reload = true
