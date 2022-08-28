0c0f209887fdfd9580901fd145e7e818e8aa3233
Justin Hurstwright <jryanhurst@gamil.com>
2022-08-27 19:30:35 -0500

    feat: introduce a sticky nav

--- app/components/navbar_component.rb
+++ app/components/navbar_component.rb
@@ -3,4 +3,15 @@
 class NavbarComponent < ViewComponent::Base
   renders_one :header
   renders_many :links
+
+  def initialize(sticky: false)
+    @sticky = sticky
+  end
+
+  private
+
+  def classnames
+    return "nav nav-sticky" if @sticky
+    "nav"
+  end
 end
--- test/components/previews/navbar_component_preview.rb
+++ test/components/previews/navbar_component_preview.rb
@@ -1,4 +1,8 @@
 class NavbarComponentPreview < Preview
   def navbar_default
   end
+
+  def navbar_sticky
+
+  end
 end
