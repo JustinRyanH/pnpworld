1d2f91eca0e52f37ee8efde42f453957e9c13528
Justin Hurstwright <jryanhurst@gamil.com>
2022-08-28 22:00:12 -0500

    feat: Introduce Alert Component

--- app/components/alert_component.rb
+++ app/components/alert_component.rb
@@ -8,7 +8,23 @@ class AlertComponent < ViewComponent::Base
   private
 
   def kind
-    return @kind.to_s if [:info, :danger, :warning, :success].include? @kind
-    'info'
+    return @kind if %i[info danger warning success].include? @kind
+
+    :info
+  end
+
+  def color
+    case kind
+    when :info
+      "text-blue-700 bg-blue-100 dark:bg-blue-200 dark:text-blue-800"
+    when :warning
+      "text-yellow-700 bg-yellow-100 dark:bg-yellow-200 dark:text-yellow-800"
+    when :success
+      "text-green-700 bg-green-100 dark:bg-green-200 dark:text-green-800"
+    when :danger
+      "text-red-700 bg-red-100 dark:bg-red-200 dark:text-red-800"
+    else
+      "text-neutral-700 bg-neutral-100 dark:bg-neutral-200 dark:text-neutral-800"
+    end
   end
 end
