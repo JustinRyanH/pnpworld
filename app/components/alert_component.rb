fa1dc7c653cd14781aee17e04053c2a52e02c724
Justin Hurstwright <jryanhurst@gamil.com>
2022-08-28 22:10:17 -0500

    ref: Create flashe messages

--- app/components/alert_component.rb
+++ app/components/alert_component.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 class AlertComponent < ViewComponent::Base
+  def self.rails_flash_to_kind(flash)
+    return :info if flash === :notice
+    return :danger if flash === :alert
+  end
+
+  # @param [Symbol] kind one of the following, `:info`, `:warning`, `:success`, `:danger`
   def initialize(kind: :info)
     @kind = kind
   end
@@ -16,15 +22,15 @@ class AlertComponent < ViewComponent::Base
   def color
     case kind
     when :info
-      "text-blue-700 bg-blue-100 dark:bg-blue-200 dark:text-blue-800"
+      'text-blue-700 bg-blue-100 dark:bg-blue-200 dark:text-blue-800'
     when :warning
-      "text-yellow-700 bg-yellow-100 dark:bg-yellow-200 dark:text-yellow-800"
+      'text-yellow-700 bg-yellow-100 dark:bg-yellow-200 dark:text-yellow-800'
     when :success
-      "text-green-700 bg-green-100 dark:bg-green-200 dark:text-green-800"
+      'text-green-700 bg-green-100 dark:bg-green-200 dark:text-green-800'
     when :danger
-      "text-red-700 bg-red-100 dark:bg-red-200 dark:text-red-800"
+      'text-red-700 bg-red-100 dark:bg-red-200 dark:text-red-800'
     else
-      "text-neutral-700 bg-neutral-100 dark:bg-neutral-200 dark:text-neutral-800"
+      'text-neutral-700 bg-neutral-100 dark:bg-neutral-200 dark:text-neutral-800'
     end
   end
 end
