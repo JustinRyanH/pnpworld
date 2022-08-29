d053d546fec711cea847e4e2e22214a3fa077d6e
Justin Hurstwright <jryanhurst@gamil.com>
2022-08-29 14:17:02 -0500

    feat: Don't allow nil users

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,5 @@
 class Character < ApplicationRecord
   belongs_to :keeper, class_name: 'User', foreign_key: 'keeper_id'
+
+  validates :name, presence: true
 end
--- test/models/character_test.rb
+++ test/models/character_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class CharacterTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'valid case' do
+    keeper = users(:example_player)
+    valid_character = Character.new(name: 'Jim', keeper:)
+    assert valid_character.valid?
+  end
+
+  test 'requires a name for the character' do
+    character_with_nil_name = Character.new(name: nil)
+    refute character_with_nil_name.valid?
+    assert_not_empty character_with_nil_name.errors[:name]
+
+    character_with_empty_string_name = Character.new(name: '')
+    refute character_with_empty_string_name.valid?
+    assert_not_empty character_with_empty_string_name.errors[:name]
+
+    character_with_whitespace_name = Character.new(name: ' ')
+    refute character_with_whitespace_name.valid?
+    assert_not_empty character_with_whitespace_name.errors[:name]
+  end
 end
