6a4d82c667eeaf767b28563387cfd2e1a828a08d
Justin Hurstwright <jryanhurst@gamil.com>
2022-08-29 14:32:36 -0500

    feat: Replace form on  bad input

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class CharactersController < ApplicationController
-  before_action :set_character, only: %i[ show edit update destroy ]
+  before_action :set_character, only: %i[show edit update destroy]
   before_action :authenticate_user!
 
   # GET /characters or /characters.json
@@ -8,8 +10,7 @@ class CharactersController < ApplicationController
   end
 
   # GET /characters/1 or /characters/1.json
-  def show
-  end
+  def show; end
 
   # GET /characters/new
   def new
@@ -17,8 +18,7 @@ class CharactersController < ApplicationController
   end
 
   # GET /characters/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /characters or /characters.json
   def create
@@ -26,12 +26,19 @@ class CharactersController < ApplicationController
 
     respond_to do |format|
       if @character.save
-        format.html { redirect_to character_url(@character), success: "Character was successfully created." }
+        format.html { redirect_to character_url(@character), success: 'Character was successfully created.' }
         format.json { render :show, status: :created, location: @character }
         format.turbo_stream
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @character.errors, status: :unprocessable_entity }
+        format.turbo_stream do
+          render(turbo_stream: turbo_stream.replace(
+            "#{helpers.dom_id(@character)}_form",
+            partial: 'form',
+            locals: { character: @character }
+          ))
+        end
       end
     end
   end
@@ -40,7 +47,7 @@ class CharactersController < ApplicationController
   def update
     respond_to do |format|
       if @character.update(character_params)
-        format.html { redirect_to character_url(@character), success: "Character was successfully updated." }
+        format.html { redirect_to character_url(@character), success: 'Character was successfully updated.' }
         format.json { render :show, status: :ok, location: @character }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -54,19 +61,20 @@ class CharactersController < ApplicationController
     @character.destroy
 
     respond_to do |format|
-      format.html { redirect_to characters_url, success: "Character was successfully destroyed." }
+      format.html { redirect_to characters_url, success: 'Character was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_character
-      @character = Character.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def character_params
-      params.require(:character).permit(:name)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_character
+    @character = Character.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def character_params
+    params.require(:character).permit(:name)
+  end
 end
