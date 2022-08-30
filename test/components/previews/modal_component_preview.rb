e3d5282a34775882a802e130ac3df9f1bbe0da1b
justinhurstwright <jryanhurst@gmail.com>
2022-08-29 20:30:52 -0500

    style: show only body

--- test/components/previews/modal_component_preview.rb
+++ test/components/previews/modal_component_preview.rb
@@ -3,11 +3,16 @@
 class ModalComponentPreview < ViewComponent::Preview
   def modal_with_body_footer_header
   end
+
   def modal_with_body_header
   end
+
   def modal_with_body
   end
 
   def modal_with_body_header_no_close
+  end
+
+  def modal_with_no_close_or_header
   end
 end
