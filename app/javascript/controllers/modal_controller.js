f7a1d83110d46858fc887f2381cdfcd15f50f74d
justinhurstwright <jryanhurst@gmail.com>
2022-09-01 11:05:34 -0600

    feat: Close modal when clicking away

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -2,10 +2,22 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="modal"
 export default class extends Controller {
+  static targets = ['mainPanel']
   connect() {
+    document.body.addEventListener('click', this.onClickAway);
+  }
+
+  disconnect() {
+    document.body.removeEventListener('click', this.onClickAway);
   }
 
   close() {
     this.element.remove();
   }
+
+  onClickAway = event => {
+    if (!this.mainPanelTarget.contains(event.target)) {
+      this.close()
+    }
+  };
 }
