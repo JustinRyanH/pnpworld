90426fcc3a3165edad4500de331429406f393a30
justinhurstwright <jryanhurst@gmail.com>
2022-09-02 08:48:01 -0600

    fix: lint

--- app/components/modal_component.rb
+++ app/components/modal_component.rb
@@ -6,7 +6,7 @@ class ModalComponent < ViewComponent::Base
   renders_one :footer
 
   # @param [Boolean] has_close_button adds the close button to the header
-  def initialize(has_close_button: true, label_id: nil, id: nil, panel_class: "")
+  def initialize(has_close_button: true, label_id: nil, id: nil, panel_class: '')
     @id = id
     @has_close_button = has_close_button
     @label_id = label_id
@@ -20,7 +20,7 @@ class ModalComponent < ViewComponent::Base
   end
 
   def content_position
-    "p-4 items-end sm:items-center sm:p-0"
+    'p-4 items-end sm:items-center sm:p-0'
   end
 
   attr_reader :has_close_button, :label_id, :id
