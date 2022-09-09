0665c97a7f8a717c83a4cdc54758b24e6e97cf8b
Justin Hurstwright <jryanhurst@gamil.com>
2022-09-09 13:07:36 -0500

    feat: Add stats

--- app/features/sheets/dungeon_world.rb
+++ app/features/sheets/dungeon_world.rb
@@ -4,5 +4,14 @@ class Sheets::DungeonWorld < Dry::Struct
   Dice = Types::String.enum('d4', 'd6', 'd8', 'd10', 'd12')
 
   attribute :max_hit_points, Types::Integer
+  attribute :level, Types::Integer
   attribute :damage, Dice
+  attribute :stats do
+    attribute :strength, Types::Integer
+    attribute :dexterity, Types::Integer
+    attribute :constitution, Types::Integer
+    attribute :intelligence, Types::Integer
+    attribute :wisdom, Types::Integer
+    attribute :charisma, Types::Integer
+  end
 end
--- test/features/sheets/dungeon_world_test.rb
+++ test/features/sheets/dungeon_world_test.rb
@@ -6,10 +6,26 @@ class DungeonWorldTest < Minitest::Test
   def test_struct
     struct = Sheets::DungeonWorld.new(
       max_hit_points: 10,
-      damage: 'd8'
+      damage: 'd8',
+      level: 1,
+      stats: {
+        strength: 16,
+        dexterity: 15,
+        constitution: 13,
+        intelligence: 12,
+        wisdom: 9,
+        charisma: 8
+      }
     )
 
     assert_equal(10, struct.max_hit_points)
     assert_equal('d8', struct.damage)
+    assert_equal(1, struct.level)
+    assert_equal(8, struct.stats.charisma)
+    assert_equal(9, struct.stats.wisdom)
+    assert_equal(12, struct.stats.intelligence)
+    assert_equal(13, struct.stats.constitution)
+    assert_equal(15, struct.stats.dexterity)
+    assert_equal(16, struct.stats.charisma)
   end
 end
