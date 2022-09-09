e237252963eba8582cff56fabaeb1ac3d7fe51a5
Justin Hurstwright <jryanhurst@gamil.com>
2022-09-09 12:41:24 -0500

    feat: Autoload dry

--- app/features/sheets/dungeon_world.rb
+++ app/features/sheets/dungeon_world.rb
@@ -1,10 +1,8 @@
-require 'dry-struct'
-
-module Types
-  include Dry.Types()
-end
+# frozen_string_literal: true
 
 class Sheets::DungeonWorld < Dry::Struct
+  Dice = Types::String.enum('d4', 'd6', 'd8', 'd10', 'd12')
+
   attribute :max_hit_points, Types::Integer
-  attribute :damage, Types::String
+  attribute :damage, Dice
 end
--- test/features/sheets/dungeon_world_test.rb
+++ test/features/sheets/dungeon_world_test.rb
@@ -1,8 +1,15 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class DungeonWorldTest < Minitest::Test
   def test_struct
-    struct = Sheets::DungeonWorld.new(max_hit_points: 10)
+    struct = Sheets::DungeonWorld.new(
+      max_hit_points: 10,
+      damage: 'd8'
+    )
+
     assert_equal(10, struct.max_hit_points)
+    assert_equal('d8', struct.damage)
   end
 end
