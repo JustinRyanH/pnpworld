2d0251b91de5aa849f289c4c60085c996e012a1d
Justin Hurstwright <jryanhurst@gamil.com>
2022-09-09 16:03:25 -0500

    ref: Extract module

---
+++ app/features/sheets/attr_stat.rb
@@ -0,0 +1,17 @@
+module Sheets::AttrStat
+  def attr_stat(kind, *attrs)
+    attrs.each do |attr|
+      instance_for_attr = "@#{attr}"
+
+      define_method(attr) do
+        instance_variable_get(instance_for_attr)
+      end
+
+      define_method("#{attr}=") do |arg|
+        return instance_variable_set(instance_for_attr, arg) if arg.is_a? kind
+
+        instance_variable_set(instance_for_attr, kind.new(value: arg))
+      end
+    end
+  end
+end
--- app/features/sheets/dungeon_world.rb
+++ app/features/sheets/dungeon_world.rb
@@ -14,24 +14,9 @@ module Sheets
 
     class Stats
       include ActiveModel::API
+      extend Sheets::AttrStat
 
-      def self.attr_stat(kind, *attrs)
-        attrs.each do |attr|
-          instance_for_attr = "@#{attr}"
-
-          define_method(attr) do
-            instance_variable_get(instance_for_attr)
-          end
-
-          define_method("#{attr}=") do |arg|
-            return instance_variable_set(instance_for_attr, arg) if arg.is_a? kind
-
-            instance_variable_set(instance_for_attr, kind.new(value: arg))
-          end
-        end
-      end
-
-      attr_stat  Stat,:charisma, :wisdom, :intelligence, :constitution, :dexterity, :strength
+      attr_stat Stat, :charisma, :wisdom, :intelligence, :constitution, :dexterity, :strength
     end
     include ActiveModel::API
 
