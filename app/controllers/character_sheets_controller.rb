5d9f5ead02ed32c13c9796b02ea6829222f32cc2
Justin Hurstwright <jryanhurst@gamil.com>
2022-09-09 20:14:45 -0500

    feat: Display Stuff

--- app/controllers/character_sheets_controller.rb
+++ app/controllers/character_sheets_controller.rb
@@ -1,30 +1,43 @@
+# frozen_string_literal: true
+
 class CharacterSheetsController < ApplicationController
   before_action :authenticate_user!
   before_action :character
-  before_action :set_character_sheet, only: %i[ show edit update destroy ]
+  before_action :set_character_sheet, only: %i[show edit update destroy]
 
   # GET /character_sheets or /character_sheets.json
   def index
     @character_sheets = character.sheets.all
+    respond_to do |format|
+      format.json { @character_sheets }
+      if @character_sheets.present?
+        format.html { render :index }
+      else
+        format.html { redirect_to new_character_sheet_url }
+      end
+    end
   end
+
+  def new; end
 
   # GET /character_sheets/1 or /character_sheets/1.json
-  def show
-  end
+  def show; end
 
   # GET /character_sheets/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /character_sheets or /character_sheets.json
   def create
     @character_sheet = CharacterSheet
-                         .create_with(character_sheet_params)
-                         .find_or_initialize_by(character: @character, keeper: current_user)
+                       .create_with(character_sheet_params)
+                       .find_or_initialize_by(character: @character, keeper: current_user)
 
     respond_to do |format|
       if @character_sheet.save
-        format.html { redirect_to character_sheets_url(@character, @character_sheet), notice: "Character sheet was successfully created." }
+        format.html do
+          redirect_to character_sheets_url(@character, @character_sheet),
+                      notice: 'Character sheet was successfully created.'
+        end
         format.json { render :show, status: :created, location: @character_sheet }
       else
         format.json { render json: @character_sheet.errors, status: :unprocessable_entity }
@@ -36,7 +49,9 @@ class CharacterSheetsController < ApplicationController
   def update
     respond_to do |format|
       if @character_sheet.update(character_sheet_params)
-        format.html { redirect_to character_sheet_url(@character_sheet), notice: "Character sheet was successfully updated." }
+        format.html do
+          redirect_to character_sheet_url(@character_sheet), notice: 'Character sheet was successfully updated.'
+        end
         format.json { render :show, status: :ok, location: @character_sheet }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -50,22 +65,22 @@ class CharacterSheetsController < ApplicationController
     @character_sheet.destroy
 
     respond_to do |format|
-      format.html { redirect_to character_sheets_url, notice: "Character sheet was successfully destroyed." }
+      format.html { redirect_to character_sheets_url, notice: 'Character sheet was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_character_sheet
-      @character_sheet = CharacterSheet.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def character_sheet_params
-      params.require(:character_sheet).permit(:keeper_id, :character_id, :level, :data, :version)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_character_sheet
+    @character_sheet = CharacterSheet.find(params[:id])
+  end
 
+  # Only allow a list of trusted parameters through.
+  def character_sheet_params
+    params.require(:character_sheet).permit(:keeper_id, :character_id, :level, :data, :version)
+  end
 
   def character
     @character = Character.find(params[:character_id])
